183c49630bd168ba24fa052451b7353600a4487d
Temitope Emokpae <70241197+temokpae3@users.noreply.github.com>
2026-06-19 07:50:41 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.14
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Music"
+print(type(variable))
+variable = [3,4,5]
+print(type(variable))
