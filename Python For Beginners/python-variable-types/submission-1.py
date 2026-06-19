dc05dba3bb6560e4dfcd9d65ec27494e06ffeb97
Temitope Emokpae <70241197+temokpae3@users.noreply.github.com>
2026-06-19 07:42:38 -0400

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,10 @@
+integer_type = 33
+float_type = 3.33
+bool_type = True
+string_type = "Melody"
+list_type = [6,7,8]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
